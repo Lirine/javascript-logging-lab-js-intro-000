c19d68f4edb205d3cd4d61f3e53f3d6db1abeb48
modern-integer-2602 <iloikov@gmail.com>
2019-10-15 17:00:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.error ("HALP!")
+<<<<<<< HEAD
 console.log("I would be a logger.")
 console.warn("I would be a swimmer") 
+=======
+console.log("I would be a logger.") 
+>>>>>>> 0cc26168ec5a728915df5a62184df90af22b086a
